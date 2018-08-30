106a5d30ef8b91dc08c51fee4e0045f28d2972aa
morgenwecks <morgenwecks@outlook.com>
2018-08-30 11:52:38 +0800

    just to save / WIP

--- basic_blockchain.py
+++ basic_blockchain.py
@@ -1,1 +1,48 @@
+# the basic blockchain
 
+import hashlib as hasher
+import datetime as date
+
+class Block:
+
+    def __init__(self,index,timestamp,data,previous_hash):
+        self.index = index
+        self.timestamp = timestamp
+        self.data = data
+        self.previous_hash = previous_hash
+        self.hash = self.hash_block()
+
+    def hash_block(self):
+        sha = hasher.sha256()
+        sha.update((str(self.index)+
+                  str(self.timestamp)+
+                  str(self.data)+
+                  str(self.previous_hash)).encode())
+        return sha.hexdigest()
+
+    @staticmethod
+    def genesis_block():
+        return Block(0,date.datetime.now(),'Genesis','0')
+
+    @staticmethod
+    def next_block(last_block):
+        this_index = last_block.index + 1
+        this_timestamp = date.datetime.now()
+        this_data = "payload" + str(this_index) # the payload can be a smart contract hashed
+        this_hash = last_block.hash
+        return Block(this_index,this_timestamp,this_data,this_hash)
+
+    def block_adder():
+        num_of_blocks_to_add = int(input('how many new blocks')) # should/could be replaced by a variable
+        for i in range(0,num_of_blocks_to_add):
+            block_to_add = next_block(previous_block)
+            blockchain.append(block_to_add)
+            previous_block = block_to_add
+            print("Block #{} has been added to blockchain".format(block_to_add.index))
+            print("Hash:{}\n".format(block_to_add.hash))
+
+def start_chain():
+    blockchain = [genesis_block()]
+    previous_block = blockchain[0]
+    return previous_block
+start_chain()
